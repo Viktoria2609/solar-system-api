6956ecd93bc287fbcc61d944810624dcf4cfcf1c
Viktoriia <lyudkevych.v@gmail.com>
2025-04-18 11:22:38 -0700

    Implemented wave 2. Created the endpoint for get one existing planet. Validated planet_list with Error handling.

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import abort, make_response, Blueprint
 from ..models.planet import planet_list
 
 
@@ -17,3 +17,29 @@ def get_all_planets():
         ))
     return result_list
 
+@planet_list_bp.get("/<id>")
+def get_one_planet(id):
+    print(f"Incoming ID: {id}")
+    planet = validate_planet(id)
+
+    planet_dict = dict(
+            id = planet.id,
+            name = planet.name,
+            description = planet.description,
+            rings = planet.rings
+            )
+    return planet_dict
+
+def validate_planet(id):
+    try:
+        id = int(id)
+    except:
+        response = {"message": f"Planet id {id} is invalid"}
+        abort(make_response(response, 400))
+    
+    for planet in planet_list:
+        if planet.id == id:
+            return planet
+        
+    not_found = {"message": f"Planet with id ({id}) not found"}
+    abort(make_response(not_found, 404))
