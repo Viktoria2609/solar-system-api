f2dea46e8c4fc74b10c465d8a48ccc0b964ab3a3
Viktoriia <lyudkevych.v@gmail.com>
2025-04-24 13:18:49 -0700

    Revert "Resolve merge conflicts and apply updated routes and config"
    This reverts commit d5766a324f9b6bfc747e68d21a9c3a86086f2499.

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,12 @@ from .routes.planet_routes import planet_list_bp
 def create_app(test_config=None):
     app = Flask(__name__)
 
-    
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
     app.register_blueprint(planet_list_bp)
 
     return app
