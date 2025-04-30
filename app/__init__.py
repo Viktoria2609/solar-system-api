f86237e535765e83b8e084228cffbae56bc60acf
Yekaterina Galochkina <galocichina.ecaterina@gmail.com>
2025-04-30 11:30:53 -0700

    Test Setup, tests for get all, get by id, post planet

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,19 @@ from flask import Flask
 from .db import db, migrate
 from .models import planet
 from .routes.planet_routes import planet_list_bp
+import os
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+
+    if test_config:
+        # Merge `config` into the app's configuration
+        # to override the app's default settings
+        app.config.update(test_config)
 
     db.init_app(app)
     migrate.init_app(app, db)
