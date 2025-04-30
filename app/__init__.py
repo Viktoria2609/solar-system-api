fb81702cd140a3e0fbfa690fa4620898725f3bcb
Viktoriia <lyudkevych.v@gmail.com>
2025-04-30 11:51:44 -0700

    Merge conflict completed

--- app/__init__.py
+++ app/__init__.py
@@ -9,12 +9,7 @@ def create_app(test_config=None):
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
-
-    if test_config:
-        # Merge `config` into the app's configuration
-        # to override the app's default settings
-        app.config.update(test_config)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
 
     db.init_app(app)
     migrate.init_app(app, db)
