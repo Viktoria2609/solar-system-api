0653f6f227ce5824d6987ca6d5078df570782b74
Yekaterina Galochkina <galocichina.ecaterina@gmail.com>
2025-04-30 11:58:49 -0700

    merge fix

--- app/__init__.py
+++ app/__init__.py
@@ -8,9 +8,12 @@ import os
 def create_app(test_config=None):
     app = Flask(__name__)
 
-
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    
+    if test_config:
+        
+        app.config.update(test_config)
 
     db.init_app(app)
     migrate.init_app(app, db)
