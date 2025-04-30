a731ccc15d19d832ab4ed99f2b26526790434824
Yekaterina Galochkina <galocichina.ecaterina@gmail.com>
2025-04-30 11:55:36 -0700

    fixing merge conflist

--- app/__init__.py
+++ app/__init__.py
@@ -8,8 +8,10 @@ import os
 def create_app(test_config=None):
     app = Flask(__name__)
 
+
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+
 
     if test_config:
         # Merge `config` into the app's configuration
